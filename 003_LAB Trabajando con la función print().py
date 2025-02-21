198a6cbab3b3629431490202f2c78b46ae3362df
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2025-02-21 06:28:49 +0000

    Practica 3

---
+++ 003_LAB Trabajando con la función print().py
@@ -0,0 +1,11 @@
+# Imprime "¡Hola, Mundo!"
+print("¡Hola, Mundo!")
+
+# Imprime mi nombre
+print("Michell")
+
+# Ejemplo de error:
+# Si eliminas las comillas, Python interpretará que es una variable no definida
+# print(¡Hola, Mundo!)  -> Error 
+# Si eliminas los paréntesis, también se generará un error
+# print "¡Hola, Mundo!"  -> Error 
