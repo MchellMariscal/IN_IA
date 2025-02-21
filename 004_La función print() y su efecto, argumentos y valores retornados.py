e4c5f3dfbd73126ab05326eaed60e44b2dd4d47c
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2025-02-21 06:32:27 +0000

    Practica 4

---
+++ 004_La función print() y su efecto, argumentos y valores retornados.py
@@ -0,0 +1,5 @@
+# Imprime un mensaje y almacena el valor de retorno de print() en la variable "resultado"
+resultado = print("Este mensaje se imprime y print() no retorna valor.")
+
+# Mostramos que el valor de retorno es None
+print("El valor de retorno de print() es:", resultado)
