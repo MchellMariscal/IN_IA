a03ccc79728add4be5f3c2ebcf280c9227aa9b8b
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2025-04-28 06:08:39 +0000

    Enfoque3_01Practica 2

---
+++ 03_Enfoque Lógica/002_Base de Conocimiento.py
@@ -0,0 +1,10 @@
+# Base de conocimiento sencilla como lista de proposiciones
+base_conocimiento = [
+    "P → Q", 
+    "Q → R", 
+    "P"
+]
+
+print("Base de Conocimiento:")
+for regla in base_conocimiento:
+    print(regla)
